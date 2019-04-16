4d1130b2fad417571262119cd2f8314b4ee79983
jcarley <jeff.carley@gmail.com>
2019-04-16 10:44:59 -0500

    Puts vs code cli on PATH

--- install-vscode.sh
+++ install-vscode.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if ! command -v code &>/dev/null; then
+  ln -s /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code /usr/local/bin/code
+fi
+
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension gerane.Theme-Jellybeams
 code --install-extension ms-vscode.csharp
