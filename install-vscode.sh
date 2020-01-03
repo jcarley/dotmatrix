f539da82c52b52f4c3a6fdb3b5574ef86ee033b0
Jeff Carley <jeff.carley@datica.com>
2020-01-03 12:06:20 -0600

    updates the extensions to install for vscode

--- install-vscode.sh
+++ install-vscode.sh
@@ -6,9 +6,18 @@ fi
 
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension gerane.Theme-Jellybeams
-code --install-extension ms-vscode.csharp
-code --install-extension octref.vetur
-code --install-extension PeterJausovec.vscode-docker
 code --install-extension quicktype.quicktype
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension vscodevim.vim
+code --install-extension deerawan.vscode-dash
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.code-runner
+code --install-extension mauve.terraform
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
+code --install-extension ms-vscode.go
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension octref.vetur
+code --install-extension pkief.material-icon-theme
+code --install-extension quicktype.quicktype
+code --install-extension shd101wyy.markdown-preview-enhanced
