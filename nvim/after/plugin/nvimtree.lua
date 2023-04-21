b37077262db930b66bd31a5a7b467794a19e5464
Jeff Carley <jeff.carley@gmail.com>
2023-04-20 21:42:11 -0500

    Updates

--- nvim/after/plugin/nvimtree.lua
+++ nvim/after/plugin/nvimtree.lua
@@ -2,7 +2,7 @@ local status, nvimtree = pcall(require, "nvim-tree")
 if (not status) then return end
 
 vim.keymap.set("n", "<leader>s", "<cmd> NvimTreeToggle <CR>")
---vim.keymap.set("n", "<leader>f", "<cmd> NvimTreeFindFile <CR>")
+vim.keymap.set("n", "<leader>f", "<cmd> NvimTreeFindFile <CR>")
 
 nvimtree.setup({
     filters = {
@@ -22,7 +22,7 @@ nvimtree.setup({
         adaptive_size = false,
         side = "left",
         width = 30,
-        hide_root_folder = true,
+        hide_root_folder = false,
         preserve_window_proportions = true,
     },
     git = {
