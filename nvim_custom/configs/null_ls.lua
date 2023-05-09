bc4f1672915971468e7810558bd64572cbc515b7
Jeff Carley <jeff.carley@gmail.com>
2023-05-09 11:56:45 -0500

    Custom configs for nvchad

---
+++ nvim_custom/configs/null_ls.lua
@@ -0,0 +1,32 @@
+local null_ls = require "null-ls"
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+null_ls.setup {
+  sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.prettier.with {
+      filetypes = { "html", "json", "yaml", "markdown" },
+    },
+    null_ls.builtins.formatting.goimports,
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format {
+            bufnr = bufnr,
+            filter = function(client)
+              return client.name == "null-ls"
+            end,
+          }
+        end,
+      })
+    end
+  end,
+}
---
+++ nvim_custom/plugins.lua
@@ -0,0 +1,65 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensured_installed = {
+        "rust_analyzer",
+        "gopls",
+        "goimports",
+        "pyright",
+        "black",
+        "flake8",
+        "isort",
+        "stylua",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspcustom"
+    end
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+    init = function()
+      require "custom.configs.null_ls"
+    end
+  },
+  {
+    "mfussenegger/nvim-dap",
+    init = function()
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+      require("core.utils").load_mappings("dap_go")
+    end
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+}
+return plugins
